55059e285de49f68eaea0992233d7413170f63cc
Suvash Budhathoki <suvash.budhathoki@intellihr.co>
2020-11-20 13:33:59 +1000

    Added mongoose and connected to it^Y

--- server/server.js
+++ server/server.js
@@ -8,7 +8,7 @@ const schema = require('./schema/schema');
 const app = express();
 
 // Replace with your mongoLab URI
-const MONGO_URI = '';
+const MONGO_URI = 'mongodb+srv://suvash:eI3STRFdNjkqHcrt@cluster0.j0e7h.mongodb.net/suvash?retryWrites=true&w=majority';
 if (!MONGO_URI) {
   throw new Error('You must provide a MongoLab URI');
 }
