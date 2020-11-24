dcda78943932396c856de1539491a0606dc3b33d
Suvash Budhathoki <suvash.budhathoki@intellihr.co>
2020-11-24 14:39:56 +1000

    complete the app

--- server/server.js
+++ server/server.js
@@ -8,7 +8,7 @@ const schema = require('./schema/schema');
 const app = express();
 
 // Replace with your mongoLab URI
-const MONGO_URI = 'mongodb+srv://suvash:eI3STRFdNjkqHcrt@cluster0.j0e7h.mongodb.net/suvash?retryWrites=true&w=majority';
+const MONGO_URI = `mongodb+srv://anu:anu1234@cluster0.el2uk.mongodb.net/anu?retryWrites=true&w=majority`;
 if (!MONGO_URI) {
   throw new Error('You must provide a MongoLab URI');
 }
